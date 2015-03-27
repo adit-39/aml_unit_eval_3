a1d9dfa2505780fb00191f85e6903a23e97735aa
adit-39 <adit.39@gmail.com>
2015-03-27 12:59:14 +0530

    Phase 1 complete

--- autoencoder.py
+++ autoencoder.py
@@ -15,58 +15,38 @@ for i in new_inputs:
 
 arr_inputs = np.asarray(inp)
 arr_outputs = np.asarray(inp)
-#arr_outputs = np.asarray([0,0,0,0,0,0,0,1])
-#print type(arr_outputs)
 training_data = zip(arr_inputs,arr_outputs)
+#test_data = arr_inputs
 
 
-print type(training_data) #list
-print type(training_data[0]) #tuple
-print type(training_data[0][0]) #ndarray
-print type(training_data[0][0][0])	#ndarray
-print type(training_data[0][0][0][0]) #float32
-
-print
-print type(training_data) #list
-print type(training_data[1]) #tuple
-print type(training_data[1][0]) #ndarray
-print type(training_data[1][0][0])	#ndarray
-print type(training_data[1][0][0][0]) #float32
-
-
-print
-print
-
-print len(training_data)
-print len(training_data[0])
-print training_data[0][0].shape
-print training_data[0][0][0].shape
-print training_data[0][0][0][0]
-
-
-print net.weights
+#print net.weights
 print "_______________________________________________________________________________\n\n"
 
-net.SGD(training_data, 30, 3, 3.0)
+net.SGD(training_data, 500, 2, 1.0)
 
-print net.weights
+#print net.weights
+#for i in net.weights:
+#	print i.shape
 print "________________________________________________________________________________\n\n"
 
-net.feedforward(training_data[0])
+'''for test,y in training_data:
+	print net.feedforward(test)
+	print ""
+'''
+op = []
+net2 = network.Network([8,3])
+net2.weights[0] = net.weights[0]
+net2.biases[0] = net.biases[0]
+for test,y in training_data:
+	op.append(net2.feedforward(test))
 
-# net2 = network.Network([8,3])
-# net2.weights = net.weights[0]
-# net2.biases = net.biases
 
-# test = np.asarray([0.,0.,0.,0.,0.,0.,0.,1.])
-# test = test.reshape((8,1))
+for v in op:
+	new = []
+	for val in v:
+		if val >=0.5:
+			new.append(1)
+		else:
+			new.append(0)
+	print new
 
-# net2.weights = np.transpose(net2.weights)
-# print test.shape
-# print net2.weights.shape
-# print
-# print
-# for row in net2.weights:
-# 	a = net2.feedforward([0.,0.,0.,0.,0.,0.,0.,1.])
-# a = net2.feedforward([0.,0.,0.,0.,0.,0.,0.,1.])
-# print a
